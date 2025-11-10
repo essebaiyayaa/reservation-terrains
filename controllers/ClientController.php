62d7a65051cadfedea04ea7e9f7be2815d0251de
Amos Nyirenda <nyirendaamos1@gmail.com>
2025-11-10 18:31:20 +0100

    prep: defined base structure (ClientController, GerantController, views/Gerant, views/Client) for integrating client and gerant functionalities

---
+++ controllers/ClientController.php
@@ -0,0 +1,32 @@
+<?php
+
+class ClientController extends BaseController{
+
+    public function index(): void{
+        
+    }
+
+   
+    public function show(string $id): void{
+
+    }
+
+   
+    public function create(): void{
+
+    }
+
+    
+    public function edit(string $id): void{
+
+    }
+
+  
+    public function delete(string $id): void{
+
+    }
+
+}
+
+
+?>
---
+++ controllers/GerantController.php
@@ -0,0 +1,31 @@
+<?php
+
+class GerantController extends BaseController {
+
+    public function index(): void{
+        
+    }
+
+   
+    public function show(string $id): void{
+
+    }
+
+   
+    public function create(): void{
+
+    }
+
+    
+    public function edit(string $id): void{
+
+    }
+
+  
+    public function delete(string $id): void{
+
+    }
+}
+
+
+?>
